ea463dba6e3365c5eda81bc7706ddfa9b9c24f8c
realthk <realthk@gmail.com>
2020-05-29 12:26:58 +0200

    fix of delays

--- tts_announce.py
+++ tts_announce.py
@@ -2,6 +2,7 @@ import pytz
 import random
 import appdaemon.plugins.hass.hassapi as hass
 import time
+import queue
 from datetime import datetime
 
 #SPEAKER = 'media_player.arpi'
@@ -13,7 +14,10 @@ TTS_SERVICE = 'tts/google_cloud_say'
 TTS_LANGUAGE = 'hu-HU'
 
 class tts_announce(hass.Hass):
+
     def initialize(self):
+        self.q_listen = queue.Queue()
+        self.q_run = queue.Queue()
         self.listen_event(self.tts_announce,  "tts_announce")        
 
     def tts_announce(self, event, data, args):
@@ -44,38 +48,49 @@ class tts_announce(hass.Hass):
 
             delay = None
             if "delay" in data:
-                delay = data.get("delay")
+                delay = data.get("delay") 
 
-            volume = 0.6
+            volume = 0.8
             if str(datetime.now().time()) > "22:00:00" or str(datetime.now().time()) < "05:00:00":
-                volume = 0.55
+                volume = 0.60
             elif str(datetime.now().time()) > "20:00:00" or str(datetime.now().time()) < "06:00:00":
-                volume = 0.55
+                volume = 0.7
             elif str(datetime.now().time()) < "07:30:00":
-                volume = 0.55
+                volume = 0.75
 
-            self.handle = None
+            self.handle = None 
             if self.get_state(SOMEONE_AT_HOME)=="on":
                 self.call_service("media_player/volume_set", entity_id=speaker, volume_level=volume)
-                if filename is not None:
-                    self.call_service("media_player/play_media", entity_id=speaker, media_content_id=filename, media_content_type="music")
-
-                if delay is not None:
-                    self.run_in(self.run_in_say_it, delay, message=text, speaker=speaker, options=options)   
-                else:                    
-                    if self.get_state(speaker)=="playing":
-                        self.handle = self.listen_state(self.listen_say_it, speaker, old = "playing", message=text, speaker=speaker, options=options)   # vagy new = "stopped" ?
+                if self.get_state(speaker)=="playing":
+                    handle = self.listen_state(self.listen_say_it, speaker, old = "playing", message=text, speaker=speaker, options=options, filename=filename, delay=delay)   # vagy new = "stopped" ?
+                    self.q_listen.put(handle)
+                else:
+                    if filename is not None:
+                        self.call_service("media_player/play_media", entity_id=speaker, media_content_id=filename, media_content_type="music")
+                        if delay is not None:
+                            timer = self.run_in(self.run_in_say_it, delay, message=text, speaker=speaker, options=options)   
+                            self.q_run.put(timer)   # handle must be put in a queue, as this app might be called again while a timer already runs
                     else:
                         self.say_it(speaker, None, None, None, message=text, speaker=speaker, options=options)
 
         else:
-            self.log("No message parameter in data")
-
-    def run_in_say_it(self, kwargs):
-        if kwargs is not None and 'message' in kwargs and 'speaker' in kwargs and 'options' in kwargs:
-            self.say_it(kwargs.get("speaker"), None, None, None, message=kwargs.get("message"), speaker=kwargs.get("speaker"), options=kwargs.get("options"))
+            self.log("No message parameter in data") 
 
     def listen_say_it(self, entity, attribute, old, new, kwargs):
+        if not self.q_listen.empty():
+            self.cancel_listen_state(self.q_listen.get())   
+
+        if kwargs is not None and 'message' in kwargs and 'speaker' in kwargs and 'options' in kwargs:
+            if 'filename' in kwargs and kwargs.get("filename") is not None:
+                self.call_service("media_player/play_media", entity_id=kwargs.get("speaker"), media_content_id=kwargs.get("filename"), media_content_type="music")
+                self.run_in(self.run_in_say_it, delay=kwargs.get("delay"), message=kwargs.get("message"), speaker=kwargs.get("speaker"), options=kwargs.get("options"))   
+            else: 
+                self.say_it(kwargs.get("speaker"), None, None, None, message=kwargs.get("message"), speaker=kwargs.get("speaker"), options=kwargs.get("options"))
+
+    def run_in_say_it(self, kwargs):
+        if not self.q_run.empty():
+            self.cancel_timer(self.q_run.get())
+
         if kwargs is not None and 'message' in kwargs and 'speaker' in kwargs and 'options' in kwargs:
             self.say_it(kwargs.get("speaker"), None, None, None, message=kwargs.get("message"), speaker=kwargs.get("speaker"), options=kwargs.get("options"))
 
@@ -84,5 +99,4 @@ class tts_announce(hass.Hass):
             text = kwargs.get("message")
             self.call_service(TTS_SERVICE, entity_id=kwargs.get("speaker"), language=TTS_LANGUAGE, message=text, options=kwargs.get("options"))
             self.log(("Saying '" + text + "'").encode('utf-8'))
-            if self.handle is not None:
-                self.cancel_listen_state(self.handle)
+ 
